ee4c4fa5c41c442143f2005a143a1a948b090e97
Pickles888 <verysaltypickles@gmail.com>
2024-07-13 18:28:19 -0700

    uncommented finished code in AnimationBase

--- src/main/java/frc/robot/subsystems/leds/animations/AnimationBase.java
+++ src/main/java/frc/robot/subsystems/leds/animations/AnimationBase.java
@@ -1,27 +1,27 @@
-// package frc.robot.subsystems.leds.animations;
+package frc.robot.subsystems.leds.animations;
 
-// import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle;
 
-// import frc.robot.Constants.LEDConstants;
-// import frc.robot.subsystems.leds.LED;
-// import frc.robot.subsystems.leds.LEDStrip;
-// import frc.robot.subsystems.leds.colors.Color;
+import frc.robot.Constants.LEDConstants;
+import frc.robot.subsystems.leds.LED;
+import frc.robot.subsystems.leds.LEDStrip;
+import frc.robot.subsystems.leds.colors.Color;
 
-// public class AnimationBase extends LEDStrip {
-//     protected AnimationBase(CANdle candle) {
-//         m_candle = candle;
+public class AnimationBase extends LEDStrip {
+    protected AnimationBase(CANdle candle) {
+        m_candle = candle;
 
-//         for (int i = 0; i <= LEDConstants.kNumOfLEDs; i++) {
-//             m_LEDStrip.get(i).add(m_blank);
-//         }
-//     }
+        for (int i = 0; i <= LEDConstants.kNumOfLEDs; i++) {
+            m_LEDStrip.get(i).add(m_blank);
+        }
+    }
 
-//     /**
-//      * @param color color of leds
-//      * @param led led that is at the center of the pattern
-//      * @param width the width of the pattern
-//      * @param gradientWidth the width of the gradient on both sides within the pattern
-//      */
+    /**
+     * @param color color of leds
+     * @param led led that is at the center of the pattern
+     * @param width the width of the pattern
+     * @param gradientWidth the width of the gradient on both sides within the pattern
+     */
 //     protected void setPointFadeInOut(Color color, int led, int width, int gradientWidth, int priority) {
 //         int solidWidth = width - (gradientWidth * 2);
 //         if (solidWidth < 0) {
@@ -38,4 +38,4 @@
 //         if (halfWidth < 0)
 //             halfWidth = 0;
 //     }
-// }
+}
